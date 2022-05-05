f9742560eebc89c96334daa288936fc2cb9df5a2
Pascal Reichert <pascal.reichert1996@gmail.com>
2022-05-05 18:09:59 +0200

    finished histogram comparison

--- color-based_retrieval.py
+++ color-based_retrieval.py
@@ -31,10 +31,28 @@ for i in range(len(h_images_q)):
 
 img_hists = []
 for i in range(len(hist_bins_images)):
-    fig, axes = plt.subplots(1, 2)
-    axes[0] = plt.imshow(rgb_images[i])
+    fig, axes = plt.subplots(2, 1)
+    axes[0].imshow(rgb_images[i])
+    axes[0].axis(False)
     hist, bins, patches = axes[1].hist(hist_bins_images[i].ravel(), bins=np.arange(0, 162, 1), density=True)
-    #plt.title(f'Image {i}')
-    fig.savefig(f'results/hist{i}.png')
+    plt.suptitle(f'Image {i + 1}')
+    fig.savefig(f'results/hist{i + 1}.png')
+    fig.tight_layout()
     plt.show()
     img_hists.append(hist)
+
+
+# Compare histograms
+# Minkowski distance between two histograms (r=1: Manhattan distance, r=2: Euclidean distance)
+def distance(hist1, hist2, r):
+    abs_diff = abs(hist1 - hist2)
+    return sum(abs_diff ** r)
+
+
+# Compare all histograms against each other with r = 1, 2
+for i in range(len(img_hists)):
+    for j in range(len(img_hists)):
+        manhattan = distance(img_hists[i], img_hists[j], 1)
+        euclidean = distance(img_hists[i], img_hists[j], 2)
+        total = manhattan + euclidean
+        print(f'image {i + 1}, image {j + 1} (r = 1): {manhattan}, (r = 2): {euclidean}, (total): {total}')
